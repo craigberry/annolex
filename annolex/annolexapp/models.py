27b5dda52aaa987833321720eb170e54908df8e8
Craig A. Berry <craig.a.berry@gmail.com>
2009-09-26 09:36:30 -0500

    Correct assorted code typos

--- annolex/annolexapp/models.py
+++ annolex/annolexapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib import admin
 
-class AnnoLex(models.Model)
+class AnnoLex(models.Model):
     KwicL = models.CharField(max_length=128)
     spell = models.CharField(max_length=40)
     KwicR = models.CharField(max_length=128)
     lempos = models.CharField(max_length=40)
     spellcolfreq = models.IntegerField()
-    wordid = models.CharField(max_length=45,primay_key=True)
+    wordid = models.CharField(max_length=45,primary_key=True)
 
 admin.site.register(AnnoLex)
