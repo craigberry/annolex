6bcf6636f270ba76bb43a524384e419a01ed513c
Craig A. Berry <craig.a.berry@gmail.com>
2009-09-26 22:34:23 -0500

    Add pagination and links to Monk.

--- annolex/annolexapp/views.py
+++ annolex/annolexapp/views.py
@@ -1,9 +1,22 @@
 from django.template import loader, Context
 from django.http import HttpResponse
 from annolex.annolexapp.models import AnnoLex
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 def start(request):
-    words = AnnoLex.objects.all()
+    word_list = AnnoLex.objects.all()
+    paginator = Paginator(word_list, 30)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        words = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        words = paginator.page(paginator.num_pages)
+
     t = loader.get_template("start.html")
     c = Context({ 'words': words })
     return HttpResponse(t.render(c))
